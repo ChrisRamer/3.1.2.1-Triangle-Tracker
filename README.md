5c945f19454cc93144bdb3e58cc05114043591a6
Chris Ramer <chrisramer.97@gmail.com>
2021-01-11 21:04:42 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# *Triangle Tracker*
+
+#### *Triangle Tracker, 12/23/2020*
+
+#### By **Chris Ramer**
+
+## Description
+
+Given 3 numbers, determines what type of triangle the numbers make, if any at all
+
+## Setup/Installation Requirements
+
+??? (haven't learnt this yet)
+
+## Specs
+
+* **Spec:** If all sides are equal, the triangle is an equilateral
+* **Input:** 2, 2, 2
+* **Output:** Equilateral
+* **Spec:** If 2 sides are equal, the triangle is an isosceles
+* **Input:** 2, 2, 3
+* **Output:** Isosceles
+* **Spec:** If no sides are equal, the triangle is scalene
+* **Input:** 2, 3, 4
+* **Output:** Scalene
+* **Spec:** If not a triangle, it is not a triangle!
+* **Input:** 2, 3, 69420
+* **Output:** NaT: Not a Triangle!
+
+## Technologies Used
+
+* C#
+
+### License
+
+Copyright (c) 2020 **Chris Ramer**
+This software is licensed under the MIT license.
---
+++ Triangle.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace TriangleTracker
+{
+	public class Triangle
+	{
+		static void Main()
+		{
+			Console.WriteLine("Please give me a number for one side of a triangle!");
+			int side1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Please give me a number for the second side of the triangle!");
+			int side2 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Please give me a number for the final side of the triangle!");
+			int side3 = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("I will now tell you the type of the triangle...");
+			Console.WriteLine(GetTriangleType(side1, side2, side3));
+		}
+
+		static string GetTriangleType(int side1, int side2, int side3)
+		{
+			if (side1 == side2 && side2 == side3)
+			{
+				return "Equilateral";
+			}
+			else if (side1 == side2 || side2 == side3 || side1 == side3)
+			{
+				return "Isosceles";
+			}
+			else if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1)
+			{
+				return  "NaT: Not a Triangle";
+			}
+			else
+			{
+				return "Scalene";
+			}
+		}
+	}
+}
